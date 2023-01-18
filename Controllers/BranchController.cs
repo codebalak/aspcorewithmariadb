122f5e3e3779c9035bd01143c8fffd6903dbe218
Santosh Kumar Rauniyar <santosh.rauniyar@icsoft.co.in>
2023-01-18 18:01:13 +0530

    AJAX Crud Book:single page application

--- Controllers/BranchController.cs
+++ Controllers/BranchController.cs
@@ -20,6 +20,8 @@ namespace aspcoremariadb.Controllers
             return View();
         }
 
+
+        //for ajax call
         [HttpPost]
         [Route("addAbranch")]
         public IActionResult AddBranch(Branch branch)
@@ -48,13 +50,14 @@ namespace aspcoremariadb.Controllers
                 message = "Added Your branch"
             });
         }
-        
+
+        [Route("branches")]
         public IActionResult Branches()
         {
             return View();
         }
 
-
+        //for ajax call
         [HttpPost]
         [Route("Allbranches")]
         public IActionResult MyBranches()
@@ -67,6 +70,85 @@ namespace aspcoremariadb.Controllers
                 message = "Branches Fetched Successfully"
             });
         }
+
+        [HttpPost]
+        [Route("singlebranch")]
+        public IActionResult GetSingleBranchDetails(Branch branch)
+        {
+            BranchRepo br = new BranchRepo();
+            var blist = br.GetSingleBranch(branch.Id);
+            if (blist != null)
+            {
+                return Json(new
+                {
+                    success = true,
+                    data = blist,
+                    message = "Record Fetched Successfully"
+                }); ;
+            }
+            else
+            {
+                return Json(new
+                {
+                    success = false,
+                    messagge="Not found"
+                });
+            }
+        }
+
+
+        [HttpPost]
+        [Route("UpdateAbranch")]
+        public IActionResult EditBranch(Branch branch)
+        {
+            if (branch.branch_name.Length < 5)
+            {
+                ModelState.AddModelError("branch_name", "Min Branch Length 5");
+            }
+            if (!ModelState.IsValid)
+            {
+
+                return Json(new
+                {
+                    success = false,
+                    errors = ModelState.Keys.SelectMany(k => ModelState[k].Errors)
+                                 .Select(m => m.ErrorMessage).ToArray()
+                });
+            }
+
+            BranchRepo br = new BranchRepo();
+            br.UpdateBranch(branch);
+
+            return Json(new
+            {
+                success = true,
+                message = "Updated Your branch"
+            });
+        }
+        [HttpPost]
+        [Route("destroy")]
+                public IActionResult RemoveBranch(Branch branchreq)
+        {
+            BranchRepo br = new BranchRepo();
+            if(br.Destroy(branchreq.Id))
+            {
+                return Json(new
+                {
+                    success = true,
+                    message = "Branch Removed Successfully"
+
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    success = false,
+                    message="Not found your branch"
+                });
+            }
+        }
+
 
     }
 }
